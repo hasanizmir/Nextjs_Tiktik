012542079cdccb7c7c1c05e95be5dd0eef8300c3
Hasan İzmir <hasanizmirr@gmail.com>
2023-01-02 17:07:59 +0300

    feat:NoResult component updated

--- components/NoResult.tsx
+++ components/NoResult.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React from "react";
+import { MdOutlineVideocamOff } from "react-icons/md";
+import { BiCommentX } from "react-icons/bi";
 
 interface IProps {
-    text: string;
+  text: string;
+  type: string;
 }
 
-const NoResult = ({text}: IProps) => {
+const NoResult = ({ text, type }: IProps) => {
   return (
-    <div>NoResult</div>
-  )
-}
+    <div className="flex flex-col justify-center items-center h-full w-full">
+      <p className="text-8xl">
+        {type === "comments" ? <BiCommentX /> : <MdOutlineVideocamOff />}
+      </p>
+      <p className="text-2xl text-center">{text}</p>
+    </div>
+  );
+};
 
-export default NoResult
+export default NoResult;
