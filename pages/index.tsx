6b612a33cfecec45ef26c379a16d252e69270fe7
Hasan İzmir <hasanizmirr@gmail.com>
2023-01-03 12:45:49 +0300

    feat:Implement topic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,42 @@
-import axios from 'axios';
-import NoResult from '../components/NoResult';
-import VideoCard from '../components/VideoCard';
-import { Video } from '../types';
-import { BASE_URL } from '../utils';
+import axios from "axios";
+import NoResult from "../components/NoResult";
+import VideoCard from "../components/VideoCard";
+import { Video } from "../types";
+import { BASE_URL } from "../utils";
 interface IProps {
-  videos: Video[]
+  videos: Video[];
 }
 
 const Home = ({ videos }: IProps) => {
   return (
     <div className="flex flex-col gap-10 videos h-full">
       {videos.length ? (
-        videos.map((video: Video) => (
-          <VideoCard post={video} key={video._id} />
-        ))
+        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
       ) : (
-          <NoResult text={'No Videos'} />
-      )}  
+        <NoResult text={"No Videos"} />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export const getServerSideProps = async () => {
-  const {data} = await axios.get(`${BASE_URL}/api/post`);
+export const getServerSideProps = async ({
+  query: { topic },
+}: {
+  query: { topic: string };
+}) => {
+  let response = null;
+
+  if (topic) {
+    response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
+  } else {
+    response = await axios.get(`${BASE_URL}/api/post`);
+  }
 
   return {
     props: {
-      videos: data
-    }
-  }
-}
+      videos: response.data,
+    },
+  };
+};
 
 export default Home;
